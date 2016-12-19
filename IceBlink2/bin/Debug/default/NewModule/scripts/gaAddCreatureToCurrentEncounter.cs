587adabe239acd331952dcf782ef0f9e32cee4e8
youngneil1 <arcanetwin@googlemail.com>
2016-12-19 19:00:42 +0100

    Just added the new script for adding creatures to encounters

---
+++ IceBlink2/bin/Debug/default/NewModule/scripts/gaAddCreatureToCurrentEncounter.cs
@@ -0,0 +1,5 @@
+//gaAddCreatureToCurrentEncounter.cs - Adds a creature to the current encounter  
+//parm1 = (string) the resref of the added creature (use one from a blueprint in the toolset's creature blueprints section)  
+//parm2 = (int) x location of the added creature on the combat map (will be automatically adjusted to nearest location if the spot is already occupied or non-walkable)  
+//parm3 = (int) y location of the added creature on the combat map (will be automatically adjusted to nearest location if the spot is already occupied or non-walkable)  
+
